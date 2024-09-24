7a9e6a4e1cd97030bbf12ba065be3ec8fabe051a
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-24 23:25:17 +0100

    Responsiveness

--- script.js
+++ script.js
@@ -37,8 +37,21 @@ function swiperAnimation() {
       });
      
 }
+page4Animation()
+swiperAnimation()
 
-
-
-  page4Animation()
-  swiperAnimation()
+var menu = document.querySelector('nav h3')
+var full = document.querySelector('#full-scr')
+var navimg = document.querySelector('nav img')
+var flag = 0
+menu.addEventListener('click',function(){
+if(flag == 0){
+    full.style.top = 0
+    navimg.style.opacity = 0
+    flag = 1
+}else{
+    full.style.top = "-100%"
+    navimg.style.opacity = 1
+    flag = 0
+}
+})
