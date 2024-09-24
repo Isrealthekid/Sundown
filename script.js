6176d330ac185a4e95407e9082db45958e082d23
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-24 12:20:42 +0100

    SwiperSection

--- script.js
+++ script.js
@@ -3,25 +3,42 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var elemC = document.querySelector("#elem-container")
-var fixed = document.querySelector("#fixed-image")
-elemC.addEventListener("mouseenter",function(){
-    fixed.style.display = "block"
-})
 
-elemC.addEventListener("mouseleave",function(){
-    fixed.style.display = "none"
-})
-
-
-
-var elems = document.querySelectorAll('.elem')
-elems.forEach(function(e){
-    e.addEventListener("mouseenter",function(){
-        var image = e.getAttribute("data-image")
-    
-        fixed.style.backgroundImage = `url(${image})`
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter",function(){
+        fixed.style.display = "block"
     })
-})
 
- 
+    elemC.addEventListener("mouseleave",function(){
+        fixed.style.display = "none"
+    })
+
+
+
+    var elems = document.querySelectorAll('.elem')
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter",function(){
+            var image = e.getAttribute("data-image")
+        
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+        
+      });
+     
+}
+
+
+
+  page4Animation()
+  swiperAnimation()
