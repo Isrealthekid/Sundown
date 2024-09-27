79faaeb3db6030c27c4cc2288c9812b5544b94ae
isrealthekid <oluwoleisreal12@gmail.com>
2024-09-27 08:29:29 +0100

    loaderAnimation

--- script.js
+++ script.js
@@ -37,21 +37,33 @@ function swiperAnimation() {
       });
      
 }
+
+
+function menuAnimation(){
+    var menu = document.querySelector('nav h3')
+    var full = document.querySelector('#full-scr')
+    var navimg = document.querySelector('nav img')
+    var flag = 0
+    menu.addEventListener('click',function(){
+    if(flag == 0){
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+    }else{
+        full.style.top = "-100%"
+        navimg.style.opacity = 1
+        flag = 0
+    }
+    })
+    
+}
+
 page4Animation()
 swiperAnimation()
+menuAnimation()
 
-var menu = document.querySelector('nav h3')
-var full = document.querySelector('#full-scr')
-var navimg = document.querySelector('nav img')
-var flag = 0
-menu.addEventListener('click',function(){
-if(flag == 0){
-    full.style.top = 0
-    navimg.style.opacity = 0
-    flag = 1
-}else{
-    full.style.top = "-100%"
-    navimg.style.opacity = 1
-    flag = 0
-}
-})
+var loader = document.querySelector("#loader")
+
+setTimeout(function(){
+    loader.style.top = "-100%"
+},4600)
